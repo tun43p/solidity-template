49fe45c9309e61d644f32a6dd1cc90e1f3b01d17
tun43p <git@tun43p.com>
2022-06-23 19:28:45 +0200

    chore: edit dotfiles and increase linting level

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,21 +9,13 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (_, hre) => {
   const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+  for (const account of accounts) console.info(account.address);
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.15",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
